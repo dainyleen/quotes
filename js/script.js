a54fd4965fe8b7ba6f9943381cf9fa73898258a3
Dainyleen Masangkay <dainyleen@gmail.com>
2021-06-05 00:08:56 +1200

    Work in progress

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//  create an array
+
+// create a function
+
+// create another another function
+
+// event listener: this is already given 
+document.getElementById('load-quote').addEventListener('click', printQuote, false)
